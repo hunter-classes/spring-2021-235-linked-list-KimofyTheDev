f57d25c1c6f08b43fc71f7698613fbedc067b08c
KimofyTheDev <imkimofy@gmail.com>
2021-02-25 00:16:13 -0500

    Updated list.cpp and added test cases in main

--- List.h
+++ List.h
@@ -1,14 +1,17 @@
 #pragma once
-
 #include "Node.h"
 
 class List{
  private:
   Node *head;
-  
+
  public:
   List();
   void insert(std::string data);
   std::string toString();
-  
+  void insert(int loc, std::string data);
+  void remove(int loc);
+  std::string get(int loc);
+  ~List();
+
 };
--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ int main(int argc, char *argv[])
   l1->insert("a");
   l1->insert("b");
   l1->insert("c");
+  l1->insert("x");
+  l1->insert("y");
+  l1->insert("z");
+  l1->insert(2,"I");
   std::cout << l1->toString() << "\n";
+  l1->remove(2);
+  std::cout << l1->toString() << "\n";
+  std::cout << l1->get(3) << "\n";
 
   return 0;
 
